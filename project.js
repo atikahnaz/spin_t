f58ed75078adbec92a32204e51cda555186a24e4
Atikah <atikahnazir6@gmail.com>
2023-07-06 12:00:44 +0800

    prompt for amount

--- project.js
+++ project.js
@@ -2,4 +2,15 @@
 
 const prompt = require('prompt-sync')(); // we can use prompt to ask user for input
 
-
+const deposit = () => {
+    while (true){
+        const depositAmount = parseFloat(prompt("Enter the amount: "));
+        if (isNaN(depositAmount) || depositAmount <= 0) {
+            console.log("Invalid number. Enter number");
+        } else{
+            return depositAmount;
+        }
+    }
+    
+};
+deposit();
